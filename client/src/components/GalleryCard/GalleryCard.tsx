1168816dc6704f0eaf70f3082416b019e234a0e3
Sarah Chung <60327675+sarahhjchung@users.noreply.github.com>
2023-11-25 16:40:46 -0500

    Fix responsive bug in Gallery page (#113)

--- client/src/components/GalleryCard/GalleryCard.tsx
+++ client/src/components/GalleryCard/GalleryCard.tsx
@@ -13,8 +13,8 @@ export function GalleryCard({ object3D }: Props) {
   return (
     <Dialog data-testid="gallery-card">
       <DialogTrigger asChild>
-        <div className="relative md:w-48 w-36 m-1 rounded-sm cursor-pointer border">
-          <div className="md:w-48 w-36 absolute inset-0 flex items-center justify-center text-center z-10 hover:bg-card text-card-foreground opacity-0 hover:opacity-70">
+        <div className="relative m-1 rounded-sm cursor-pointer border">
+          <div className="absolute inset-0 flex items-center justify-center text-center z-10 hover:bg-card text-card-foreground opacity-0 hover:opacity-70">
             <p className="font-mono p-2">{object3D.prompt}</p>
           </div>
           <img

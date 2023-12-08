9e7f72199201a74eed9395c7bef5c2aa6990697e
Steven Mathew <ste.tho.mat@gmail.com>
2023-12-08 15:38:48 -0500

    feat: add port to ssh-configure-keys

--- dev/ssh-configure-keys.sh
+++ dev/ssh-configure-keys.sh
@@ -6,12 +6,13 @@ unset -v github_email
 
 ssh_key_path=$HOME/.ssh/id_rsa.pub
 
-while getopts u:s:e: flag
+while getopts u:s:e:p: flag
 do
     case "${flag}" in
         u) username=${OPTARG};;
         s) server=${OPTARG};;
         e) github_email=${OPTARG};;
+        p) port="${OPTARG:-22}";;
     esac
 done
 
@@ -24,12 +25,12 @@ echo "Adding $server fingerprint..."
 ssh-keyscan -H $server >> ~/.ssh/known_hosts
 
 echo "Copying SSH key to $server..."
-ssh-copy-id -i $ssh_key_path -p 2233 $username@$server
+ssh-copy-id -i $ssh_key_path -p $port $username@$server
 
 echo "Creating new SSH key on $username@$server..."
-ssh -p 2233 $username@$server "ssh-keygen -t rsa -b 4096 -P '' -C '${github_email}'"
+ssh -p $port $username@$server "ssh-keygen -t rsa -b 4096 -P '' -C '${github_email}'"
 
 echo "Getting public SSH key on $username@$server..."
-ssh -p 2233 $username@$server "cat ~/.ssh/id_rsa.pub"
+ssh -p $port $username@$server "cat ~/.ssh/id_rsa.pub"
 
 echo "Add this SSH public key to your account on GitHub."
